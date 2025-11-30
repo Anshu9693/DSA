514f89d5806587ee1a669ddb419c7d7e2dfe02c6
Anshu9693 <anshukumarsah8585@gmail.com>
2025-12-01 05:04:30 +0530

    Leetcode129

---
+++ Trees/LeetCode129.java
@@ -0,0 +1,22 @@
+public class LeetCode129 {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int currentSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        currentSum = currentSum * 10 + node.val;
+
+        // If it's a leaf node, return the current sum
+        if (node.left == null && node.right == null) {
+            return currentSum;
+        }
+
+        // Recur for left and right subtrees
+        return dfs(node.left, currentSum) + dfs(node.right, currentSum);
+    }
+    
+}
